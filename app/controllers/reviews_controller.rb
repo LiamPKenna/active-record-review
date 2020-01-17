74d28011b148444514a9b68c55ebe54410218ffd
LiamPKenna <liampkenna@gmail.com>
2020-01-17 13:19:52 -0800

    Add review routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,31 +4,51 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
   end
 
   def create
-    
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "#{@review.author} successfully added a review!"
+      redirect_to product_path
+    else
+      render :new
+    end
   end
 
   def show
-    
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
   end
 
   def edit
-    
+    @product = Product.find(params[:id])
+    render :edit
   end
 
   def update
-    
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "#{@review.author} successfully updated their post!"
+      redirect_to product_review_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
   end
 
   private
-  def review_parameters
-    
+  def review_params
+    params.require(:review).permit(:author, :rating, :content_body)
   end
 end
