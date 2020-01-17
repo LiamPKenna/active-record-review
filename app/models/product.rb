da204fc51f9295ed34b7236c7ca53f3f25978cd2
LiamPKenna <liampkenna@gmail.com>
2020-01-17 09:55:00 -0800

    Add scope to order products by number of reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(10)
+  )}
 end
