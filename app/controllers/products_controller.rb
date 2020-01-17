12361e837beafecbe5e38b379818da633620910a
LiamPKenna <liampkenna@gmail.com>
2020-01-17 11:36:03 -0800

    Add edit, show, & update paths/views for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,8 +17,36 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = "#{@product.name} successfully added!"
+      redirect_to products_path
     else
       render :new
     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @reviews = @product.reviews
+    render :show
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "#{@product.name} successfully updated!"
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+
+  private 
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
   end
 end
