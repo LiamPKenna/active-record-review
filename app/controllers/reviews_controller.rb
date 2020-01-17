611b630122f5508c0407b39db44a67b255d24301
LiamPKenna <liampkenna@gmail.com>
2020-01-17 12:31:22 -0800

    Add skeleton of reviews routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,33 @@ class ReviewsController < ApplicationController
   def index
     render :index
   end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def review_parameters
+    
+  end
 end
