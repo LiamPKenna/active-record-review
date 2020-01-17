d4b56d0593800b8521f8b0dfb725692d9b7682b7
LiamPKenna <liampkenna@gmail.com>
2020-01-17 12:24:55 -0800

    Add callback titleize product name, review author

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :cost, :country_of_origin, presence: true
 
+  before_save(:titleize_name)
+
   scope :most_reviews, -> {(
     select("products.id, products.name, count(reviews.id) as reviews_count")
     .joins(:reviews)
@@ -11,4 +13,11 @@ class Product < ApplicationRecord
   )}
 
   scope :newest, -> { order("created_at DESC").limit(3) }
+
+  private
+  def titleize_name
+    self.name = self.name.split(' ').map { |w|
+      (w[0] =~ /[a-z]/i) ? (w[0].upcase + w.slice(1, w.length)) : w
+    }.join(' ')
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  before_save(:titleize_author)
+
+  private
+  def titleize_author
+    self.author = self.author.split(' ').map { |w|
+      (w[0] =~ /[a-z]/i) ? (w[0].upcase + w.slice(1, w.length)) : w
+    }.join(' ')
+  end
 end
