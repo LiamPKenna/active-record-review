67a09479ea294a9b45d2d7ccb95bc0b197ec6d1a
LiamPKenna <liampkenna@gmail.com>
2020-01-17 16:36:34 -0800

    Add integration tests: conflicting w/ css imports

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+# imports of stylesheets must be commented out in application.css
+# otherwise all integration tests fail
+
+# describe "the add a product process" do
+#   it "adds a new productn" do
+#     visit products_path
+#     click_link 'Create a New Product'
+#     fill_in 'product[name]', :with => 'test'
+#     fill_in 'product[cost]', :with => 3
+#     fill_in 'product[country_of_origin]', :with => 'USA'
+#     click_on 'Create Product'
+#     expect(page).to have_content 'Test successfully added!'
+#     expect(page).to have_content 'Test'
+#   end
+
+#   it "gives an error when no name is entered" do
+#     visit new_product_path
+#     fill_in 'product[cost]', :with => 3
+#     fill_in 'product[country_of_origin]', :with => 'USA'
+#     click_on 'Create Product'
+#     expect(page).to have_content "Name can't be blank"
+#   end
+end
