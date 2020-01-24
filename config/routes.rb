cee17bd24e80d22edb5d94fb58e11487484ab442
LiamPKenna <liampkenna@gmail.com>
2020-01-24 10:02:38 -0800

    Add routes for signing in and out

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
