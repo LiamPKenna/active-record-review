68deba348a3eb57c6454f7ff1d4314548d2e95a6
LiamPKenna <liampkenna@gmail.com>
2020-01-24 13:22:10 -0800

    Coplete create user tests for 100% coverage

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 describe "the add a product process" do
 
   before(:each) do
-    
+    users = User.all
+    users.each { |u| u.destroy }
   end
 
   it "allows a user to create a new account" do
@@ -15,6 +16,15 @@ describe "the add a product process" do
     check 'user[admin]'
     click_on 'Sign Up'
     expect(page).to have_content "test@test.com"
+  end
+
+  it "requires a user to complete all fields" do
+    visit "/signup"
+    fill_in 'user[password]', :with => 'test@test.com'
+    fill_in 'user[password_confirmation]', :with => 'test@test.com'
+    check 'user[admin]'
+    click_on 'Sign Up'
+    expect(page).to have_content "There was a problem signing up."
   end
 
 end
